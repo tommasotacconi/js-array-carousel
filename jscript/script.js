4ebf3d2d4c7b29d4106e81ee2f2b933582515e5a
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-20 17:41:26 +0200

    added carousel imgs dynamically

--- jscript/script.js
+++ jscript/script.js
@@ -1,1 +1,16 @@
+// SET UP PHASE
+//1. Preparo le sorgenti delle immagini
+const sources = ['1', '2', '3', '4', '5', '6', '7', '8'];
+//2. Prendo gli elementi che mi interessano dal DOM
+const carouselGallery = document.querySelector('#gallery');
+console.log(carouselGallery);
 
+//2. Genero le immagini da js
+for (let i = 0; i < sources.length; i++) {
+  const src = `img/moto-${sources[i]}.jpg`;
+  const image = document.createElement('img');
+  image.src = src;
+  image.alt = `moto-${sources[i]}`;
+  carouselGallery.appendChild(image);
+  console.log(image);
+}
