5550da0c55e90b56fd363acb96e704596c709af8
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-20 17:59:09 +0200

    added js to slide carousel with arrow

--- jscript/script.js
+++ jscript/script.js
@@ -4,13 +4,50 @@ const sources = ['1', '2', '3', '4', '5', '6', '7', '8'];
 //2. Prendo gli elementi che mi interessano dal DOM
 const carouselGallery = document.querySelector('#gallery');
 console.log(carouselGallery);
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
 
-//2. Genero le immagini da js
+
+//3. Genero le immagini da js
 for (let i = 0; i < sources.length; i++) {
   const src = `img/moto-${sources[i]}.jpg`;
   const image = document.createElement('img');
   image.src = src;
   image.alt = `moto-${sources[i]}`;
   carouselGallery.appendChild(image);
-  console.log(image);
 }
+
+//4. Costruisco la lista di nodi immagine
+//in modo da poter agire poi sulle singole immagini
+const images = document.querySelectorAll('#carousel img');
+console.log(images);
+
+//5. Rendo visibile la prima immagine
+let activeIndex = 0;
+images[activeIndex].classList.add('active');
+
+//EVENTI DINAMICI
+//6. Reazione al next
+nextButton.addEventListener('click', function () {
+  //tolgo la classe dall'immagine con classe active
+  images[activeIndex].classList.remove('active');
+  
+  //incremento l'indice
+  activeIndex++;
+  
+  //aggiungo la classe all'immagine successiva a quella active
+  images[activeIndex].classList.add('active');
+})
+
+//7. Reazione al prev
+prevButton.addEventListener('click', function () {
+  //tolgo la classe dall'immagine con classe active
+  images[activeIndex].classList.remove('active');
+  
+  //decremento l'indice
+  activeIndex--;
+  
+  //aggiungo la classe all'immagine precedente a quella active
+  images[activeIndex].classList.add('active');
+})
+
